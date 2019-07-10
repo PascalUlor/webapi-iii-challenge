43adca9581cb20ab473fc45903644d3021156b26
PascalUlor <pascalulor@yahoo.com>
2019-07-10 12:39:19 +0100

    setup express server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+const app = require("./server.js");
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = 'express';
+const express = require("express");
+const helmet = require("helmet");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function logger(req, res, next) {}
 
 module.exports = server;
